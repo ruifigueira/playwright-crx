5d31a92e2d679691b59deba6f255434436c81254
Rui Figueira <rui.figueira@gmail.com>
2023-11-04 13:50:26 +0000

    tests: change default workers to 2

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 2,
   reporter: 'html',
   use: {
     baseURL: 'http://127.0.0.1:3000',
