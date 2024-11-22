53a1b7c8bfcdad345f0cfa979ea901cff7ed48b0
Rui Figueira <rui.figueira@gmail.com>
2024-11-22 13:33:08 +0000

    wip: sw support for live tracing

--- examples/recorder-crx/src/sw/crxTraceModelBackend.ts
+++ examples/recorder-crx/src/sw/crxTraceModelBackend.ts
@@ -1,6 +1,7 @@
 import type * as zip from '@zip.js/zip.js';
 import type { TraceModelBackend } from '@trace-viewer/sw/traceModel';
 import * as zipjs from '@zip.js/zip.js';
+import { type Crx, crx } from 'playwright-crx';
 
 type Progress = (done: number, total: number) => undefined;
 
@@ -59,5 +60,83 @@ export class CrxZipTraceModelBackend implements TraceModelBackend {
     const writer = new zipjs.BlobWriter() as zip.BlobWriter;
     await entry.getData!(writer);
     return writer.getData();
+  }
+}
+
+export class CrxFetchTraceModelBackend implements TraceModelBackend {
+  private _fs: Crx['fs'];
+  private _traceURL: string;
+  private _entries: Map<string, string>;
+  private _json: string;
+
+  constructor(traceURL: string) {
+    const fs = this._fs = crx.fs;
+    this._traceURL = traceURL;
+
+    function traceDescriptor(traceName: string) {
+      const result: { entries: { name: string, path: string }[] } = {
+        entries: []
+      };
+    
+      const tracePrefix = traceName.replace('/test-results/.playwright-artifacts-0', '/tmp').replace(/\.json$/, '');
+
+      const traceDir = '/tmp/traces';
+      const traceFile = tracePrefix.substring(traceDir.length + 1);
+      for (const name of fs.readdirSync(traceDir) as string[]) {
+        if (name.startsWith(traceFile))
+          result.entries.push({ name, path: `${traceDir}/${name}` });
+      }
+    
+      const resourcesDir = `${traceDir}/resources`;
+      if (fs.existsSync(resourcesDir)) {
+        for (const name of fs.readdirSync(resourcesDir))
+          result.entries.push({ name: 'resources/' + name, path: `${resourcesDir}/${name}` });
+      }
+      return result;
+    }
+
+    const json = traceDescriptor(traceURL);;
+    this._json = JSON.stringify(traceDescriptor(traceURL));
+    this._entries = new Map<string, string>();
+    for (const entry of json.entries)
+      this._entries.set(entry.name, entry.path);
+  }
+
+  isLive() {
+    return true;
+  }
+
+  traceURL(): string {
+    return this._traceURL;
+  }
+
+  async entryNames(): Promise<string[]> {
+    return [...this._entries.keys()];
+  }
+
+  async hasEntry(entryName: string): Promise<boolean> {
+    return this._entries.has(entryName);
+  }
+
+  async readText(entryName: string): Promise<string | undefined> {
+    if (entryName === this._traceURL)
+      return this._json;
+    const file = this._entries.get(entryName);
+    let content: string | undefined;
+    try {
+      if (file)
+        content = this._fs.readFileSync(file, { encoding: 'utf-8' }) as string;
+    } catch {}
+    return content;
+  }
+
+  async readBlob(entryName: string): Promise<Blob | undefined> {
+    const file = this._entries.get(entryName);
+    let blob: Blob | undefined;
+    try {
+      if (file)
+        blob = new Blob([this._fs.readFileSync(file)]);
+    } catch {}
+    return blob;
   }
 }
