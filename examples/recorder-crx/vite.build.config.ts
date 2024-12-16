eb4847121ab8ed8eb7552f0d11e2de581e8a81a3
Rui Figueira <rui.figueira@gmail.com>
2024-12-16 11:11:09 +0000

    chore: hmr with vite-plugin-web-extension

---
+++ examples/recorder-crx/vite.build.config.ts
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) Rui Figueira.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import fs from 'fs';
+import path from 'path';
+import sourcemaps from 'rollup-plugin-sourcemaps';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [{
+    name: 'copy-recorder-crx-manifest',
+    closeBundle: () => {
+      fs.copyFileSync(
+          path.join(__dirname, 'manifest.json'),
+          path.join(__dirname, 'dist', 'manifest.json'));
+    },
+  }],
+  resolve: {
+    alias: {
+      '@isomorphic': path.resolve(__dirname, '../../playwright/packages/playwright-core/src/utils/isomorphic'),
+      '@protocol': path.resolve(__dirname, '../../playwright/packages/protocol/src'),
+      '@web': path.resolve(__dirname, '../../playwright/packages/web/src'),
+      '@recorder': path.resolve(__dirname, '../../playwright/packages/recorder/src'),
+    },
+  },
+  build: {
+    // skip code obfuscation
+    minify: false,
+    // chunk limit is not an issue, this is a browser extension
+    chunkSizeWarningLimit: 10240,
+    sourcemap: true,
+    rollupOptions: {
+      // @ts-ignore
+      plugins: [sourcemaps()],
+      input: {
+        'index': path.resolve(__dirname, 'index.html'),
+        'preferences': path.resolve(__dirname, 'preferences.html'),
+        'background': path.resolve(__dirname, 'src/background.ts'),
+      },
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
+      },
+    },
+  },
+});
